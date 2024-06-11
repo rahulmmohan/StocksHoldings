a01095a1bfdc9c40c2ee6cd20cb7f4b16c1369a5
rahulmmohan <-g>
2024-06-11 14:06:15 +0530

    some test cases

--- __tests__/StockUtils-test.tsx
+++ __tests__/StockUtils-test.tsx
@@ -33,11 +33,25 @@ describe('calculatePandL', () => {
     expect(StocksUtils.calculatePandL(100, 10)).toEqual(90);
   });
 
-  it('should return 12340 for currentValue:1234 and investmentValue:10 ', () => {
+  it('should return 1224 for currentValue:1234 and investmentValue:10 ', () => {
     expect(StocksUtils.calculatePandL(1234, 10)).toEqual(1224);
   });
 
-  it('should return 1000 for currentValue:500 and investmentValue:200 ', () => {
+  it('should return 300 for currentValue:500 and investmentValue:200 ', () => {
     expect(StocksUtils.calculatePandL(500, 200)).toEqual(300);
+  });
+});
+
+describe('calculateTodayPandL', () => {
+  it('should return 50000 for close:15000, ltp:10000 and quantity:10 ', () => {
+    expect(StocksUtils.calculateTodayPandL(15000, 10000, 10)).toEqual(50000);
+  });
+
+  it('should return 7660 for close:2000, ltp:1234 and quantity:10 ', () => {
+    expect(StocksUtils.calculateTodayPandL(2000, 1234, 10)).toEqual(7660);
+  });
+
+  it('should return 1000 for close:1000, ltp:500 and quantity:2 ', () => {
+    expect(StocksUtils.calculateTodayPandL(1000, 500, 2)).toEqual(1000);
   });
 });
